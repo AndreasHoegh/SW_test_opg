26b49f1e28df3d8b8dbb98a6ee677e26c7c7f52d
Andreas Høgh <au639339@uni.au.dk>
2022-02-03 13:36:56 +0100

    Add project files.

---
+++ SW_test_opg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SW_test_opg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            
+            Console.WriteLine(calc.Add(3, 2));
+        }
+    }
+}
---
+++ TestProject1/CalculatorUnitTests.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using SW_test_opg;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        private Calculator calc;
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+
+            calc = new Calculator();
+        }
+
+        [Test]
+        public void Test_Add()
+        {
+
+            //Act
+            double result = calc.Add(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Test_Subtract()
+        {
+
+            //Act
+            double result = calc.Subtract(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Test_Multiply()
+        {
+
+            //Act
+            double result = calc.Multiply(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
