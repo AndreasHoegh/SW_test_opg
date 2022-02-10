10522a1b8c3d258583c5038549c6b875e88cf861
Abdul1997 <201711212@post.au.dk>
2022-02-10 12:19:05 +0100

    a

--- SW_test_opg/Calculator.cs
+++ SW_test_opg/Calculator.cs
@@ -3,7 +3,7 @@ namespace SW_test_opg
 {
     public class Calculator
     {
-
+        private double result;
 
         public double Add(double a, double b)
         {
@@ -21,5 +21,6 @@ namespace SW_test_opg
         {
             return Math.Pow(x, exp);
         }
+
     }
 }
